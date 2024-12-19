749225faff28acf30e1cb9c8143788ac0b5f5554
silver-eva <haushki.74.7@gmail.com>
2024-12-19 17:03:11 +0200

    feat: environment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import uvicorn
 import argparse
 
+from src.app import App
+from src.lib.config import LOGGING_CONFIG
+
 def get_args():
     parser = argparse.ArgumentParser(description="Process some arguments.")
     parser.add_argument('-a', '--address', type=str, help='IP address to bind to', default='0.0.0.0')
@@ -11,16 +14,25 @@ def get_args():
 
     return parser.parse_args()
 
-def main():
-    args = get_args()
-    uvicorn.run(
-        "app:app", 
-        host=args.address, 
-        port=args.port, 
-        reload=args.reload, 
-        log_level=args.log_level
-    )
-
+app = App()
 
 if __name__ == "__main__":
-    main()
+    args = get_args()
+
+    async def startup_event():
+        print("Startup")
+
+    async def shutdown_event():
+        print("Shutdown")
+    
+    app.add_event_handler("startup", startup_event)
+    app.add_event_handler("shutdown", shutdown_event)
+
+    uvicorn.run(
+        "main:app",
+        host=args.address,
+        port=args.port,
+        reload=args.reload,
+        log_level=args.log_level,
+        log_config=LOGGING_CONFIG
+    )
