9e1e26a73654dd64c703b766946ea58d52f437e5
silver-eva <haushki.74.7@gmail.com>
2024-12-28 00:27:53 +0200

    feat: crud for categoryes and characteristics

--- src/controllers/coreController.py
+++ src/controllers/coreController.py
@@ -1,30 +1,25 @@
-from fastapi import APIRouter, status, Depends, Query, HTTPException
+from fastapi import APIRouter, status, Depends, Query, HTTPException, Path ,Body
 from datetime import datetime
+from typing import Literal
+from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.engine import get_session, db_execute
 from src.db.models import Category, Characteristic, BaseModel
 
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy import select, func
+from sqlalchemy import select, func, or_, delete
 from sqlalchemy.inspection import Callable
 
 from src.models.requests import ListRequest, CoreRequest
 from src.models.responses import ListResponse, CoreResponse
 
+CORE_PARAMS = Literal["category", "characteristic"]
 
 async def add_or_update_core(db: AsyncSession, model: BaseModel, request: CoreRequest) -> None:
-    result = await db_execute(
-        db, 
-        select(
-            model
-        ).where(
-            model.name == request.name
-        ), 
-        with_result="one")
-
-    if result:
-        return HTTPException(status.HTTP_409_CONFLICT, f"{model.__tablename__} already exists")
+    if not request.id:
+        if await db_execute(db, select(model).where(model.name == request.name), with_result="one"):
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "Name already exists")
     
     await db_execute(
         db,
@@ -58,7 +53,12 @@ async def list_core(
         request.limit
     ).order_by(order).add_columns(
         func.count(model.id).over().label("total")
-    ).group_by(model.name, model.id)
+    ).group_by(model.name, model.id).where(
+        or_(
+            model.name.icontains(request.keyword),
+            model.description.icontains(request.keyword)
+        )
+    )
 
     core = await db_execute(db, query, with_result="raw_all")
 
@@ -74,24 +74,44 @@ async def list_core(
     )
     return response
 
+async def remove_core(db, model, id):
+    await db_execute(db, delete(model).where(model.id == id))
+    return status.HTTP_204_NO_CONTENT
+
 class CoreController(APIRouter):
     def __init__(self):
         super().__init__(prefix="/core", tags=["Core"])
+        self.param_map = {
+            "category": Category,
+            "characteristic": Characteristic
+        }
 
-        self.add_api_route("/categoryes/list", self.list_category, methods=["GET"], description="List category", response_model=ListResponse)
-        self.add_api_route("/categoryes/", self.add_or_update_category, methods=["PATCH"], description="Add or update category", response_model=None)
+        self.add_api_route("/list/{param}", self.list_param, methods=["GET"], description="List parameters", response_model=ListResponse)
+        self.add_api_route("/{param}/", self.add_or_update_param, methods=["PATCH"], description="Add or update Item parameter", response_model=None)
+        self.add_api_route("/{param}/{id}", self.remove_param, methods=["DELETE"], description="Remove Item parameter", response_model=None)
 
-        self.add_api_route("/characteristics/list", self.list_characteristic, methods=["GET"], description="List characteristic", response_model=ListResponse)
-        self.add_api_route("/characteristics/", self.add_or_update_characteristic, methods=["PATCH"], description="Add or update characteristic", response_model=None)
-
-    async def list_category(self, request: ListRequest = Query(default=ListRequest()), db: AsyncSession = Depends(get_session)) -> ListResponse:
-        return await list_core(db, Category, Category.updated_at.desc(), request)
+    async def list_param(
+            self, 
+            param: CORE_PARAMS = Path(), 
+            request: ListRequest = Query(default=ListRequest()), 
+            db: AsyncSession = Depends(get_session)
+            ) -> ListResponse:
+        
+        return await list_core(db, self.param_map[param], self.param_map[param].name.asc(), request)
     
-    async def add_or_update_category(self, request: CoreRequest, db: AsyncSession = Depends(get_session)):
-        return await add_or_update_core(db, Category, request)
+    async def add_or_update_param(
+            self, 
+            param: CORE_PARAMS = Path(), 
+            request: CoreRequest = Body(default=CoreRequest()), 
+            db: AsyncSession = Depends(get_session)
+        ):
+        return await add_or_update_core(db, self.param_map[param], request)
     
-    async def add_or_update_characteristic(self, request: CoreRequest, db: AsyncSession = Depends(get_session)):
-        return await add_or_update_core(db, Characteristic, request)
+    async def remove_param(
+            self, 
+            param: CORE_PARAMS = Path(), 
+            id: UUID = Path(), 
+            db: AsyncSession = Depends(get_session)
+        ):
+        return await remove_core(db, self.param_map[param], id)
     
-    async def list_characteristic(self, request: ListRequest = Query(default=ListRequest()), db: AsyncSession = Depends(get_session)) -> ListResponse:
-        return await list_core(db, Characteristic, Characteristic.name.asc(), request)
