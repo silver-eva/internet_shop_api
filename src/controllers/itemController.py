83e02220a29cff59fbe680174a551dcdaa513645
silver-eva <haushki.74.7@gmail.com>
2024-12-28 00:28:16 +0200

    feat: upsert + get item

--- src/controllers/itemController.py
+++ src/controllers/itemController.py
@@ -1,76 +1,43 @@
-from fastapi import APIRouter, Query, Path, Depends, Body, status, HTTPException
-from typing import Literal
+from fastapi import APIRouter, Path, Depends, status, HTTPException
 from datetime import datetime
+from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.engine import get_session, db_execute
-from src.db.models import Item, Category, Characteristic
+from src.db.models import Item, Category, Characteristic, ItemCharacteristic, ItemCategory, Review
 
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy import select
-# from src.db.service.item import insert_item, list_items
+from sqlalchemy import select, delete
 
-from src.models.itemModels import ItemRequest
-
+from src.models.requests import ItemRequest
+from src.models.responses import ItemResponse, ItemCategoryResponse, CoreResponse, ReviewResponse
 
 
 class ItemController(APIRouter):
     def __init__(self):
         super().__init__(prefix = "/items", tags = ["Items"])
 
-        # self.add_api_route("/list/{category}", self.get_items, methods=["GET"], description="List items")
-        self.add_api_route("/", self.create_item, methods=["POST"], description="Create item", response_model=None)
+        self.add_api_route("/", self.create_item, methods=["PATCH"], description="Create item", response_model=None)
+        self.add_api_route("/{id}", self.get_item, methods=["GET"], description="Get item", response_model=ItemResponse)
 
-    async def get_items(
-            self,
-            category: str = Path(...),
-            filter: str = Query(default=""),
-            limit: int = Query(default=10, gt=0),
-            page: int = Query(default=1, gt=0),
-            db: AsyncSession = Depends(get_session)
-            ) -> list[Item]:
-        
-        # TODO: Item with category(Category model), reviews (List[Review]), characteristics (List[Characteristic])
 
-        offset = (page - 1) * limit
-        query = select(Item).limit(limit).offset(offset)
-
-        # filter by category
-        query = query.where(Item.category == category)
-
-        # filter by other filters
-        for key, value in filter.items():
-            query = query.where(getattr(Item, key) == value)
-
-        items = await db_execute(db, query, with_result="all")
-
-        return items
-    
-    async def get_item(
-            self, 
-            id: int = Path(), 
-            lang: Literal["ua", "en", "ru"] = Query(default="ua"),
-            curency: Literal["uah", "usd"] = Query(default="uah"),
-            db: AsyncSession = Depends(get_session)
-            ) -> dict:
-        return {}
-    
     async def create_item(
             self, 
             item: ItemRequest,
             db: AsyncSession = Depends(get_session),
             ) -> None:
 
-        category = (await db.execute(
-            select(
-                Category
-            ).where(
-                Category.id == item.category_id
-            )
-        )).scalar_one_or_none()
-
+        category = await db_execute(db, select(Category).where(Category.id == item.category_id), with_result="one")
         if not category:
             raise HTTPException(status.HTTP_404_NOT_FOUND, "Category not found")
+        
+        if item.id:
+            await db.execute(
+                delete(ItemCharacteristic).where(ItemCharacteristic.item_id == item.id)
+            )
+            await db.execute(
+                delete(ItemCategory).where(ItemCategory.item_id == item.id)
+            )
 
         new_item = (await db.execute(
             insert(Item).values(
@@ -78,57 +45,85 @@ class ItemController(APIRouter):
                 name=item.name,
                 description=item.description,
                 price=item.price,
-                category_id=category.id,
             ).on_conflict_do_update(
                 index_elements=[Item.id],
                 set_={
                     Item.name: item.name,
                     Item.description: item.description,
                     Item.price: item.price,
-                    Item.category_id: category.id,
                     Item.updated_at: datetime.now()
                 }
             ).returning(Item)
-        )).scalar_one_or_none()
+        )).scalar()
 
-        if new_item is None:
-            raise HTTPException("Item not created", status_code=status.HTTP_400_BAD_REQUEST)
+        await db.execute(
+            insert(ItemCategory).values(
+                item_id=new_item.id,
+                category_id=item.category_id
+            )
+        )
 
         for characteristic in item.characteristics:
+            if not await db.execute(select(Characteristic).where(Characteristic.id == characteristic.id)):
+                raise HTTPException(status.HTTP_404_NOT_FOUND, f"Characteristic {characteristic.name} not found")
             await db.execute(
-                insert(Characteristic).values(
-                    value=characteristic.value,
+                insert(ItemCharacteristic).values(
                     item_id=new_item.id,
-                    name = characteristic.name,
-                    description = characteristic.description
-                ).on_conflict_do_update(
-                    index_elements=[Characteristic.name],
-                    set_={
-                        Characteristic.name: characteristic.name,
-                        Characteristic.description: characteristic.description,
-                        Characteristic.value: characteristic.value,
-                        Characteristic.item_id: new_item.id,
-                        Characteristic.updated_at: datetime.now()
-                    }
+                    characteristic_id=characteristic.id,
+                    value=characteristic.value
                 )
             )
 
         await db.commit()
         return status.HTTP_201_CREATED
     
-    async def update_item(
+    async def get_item(
             self, 
-            id: int = Path(), 
-            item: dict = Body(default={}),
-            lang: Literal["ua", "en", "ru"] = Query(default="ua"),
-            curency: Literal["uah", "usd"] = Query(default="uah"),
-            ) -> None:
-        return None
-    
-    async def delete_item(
-            self, 
-            id: int = Path(),
-            lang: Literal["ua", "en", "ru"] = Query(default="ua"),
-            curency: Literal["uah", "usd"] = Query(default="uah"),
-            ) -> None:
-        return None
+            id: UUID = Path(),
+            db: AsyncSession = Depends(get_session)
+            ) -> ItemResponse:
+        item = await db_execute(db, select(Item).where(Item.id == id), with_result="one")
+        category = await db_execute(
+            db, 
+            select(
+                Category
+            ).select_from(ItemCategory).where(
+                ItemCategory.item_id == id
+            ),
+            with_result="one"
+        )
+        reviews = await db_execute(
+            db, 
+            select(
+                Review
+            ).where(
+                Review.item_id == id
+            ),
+            with_result="all"
+        )
+        characteristics = await db_execute(
+            db, 
+            select(
+                Characteristic
+            ).select_from(ItemCharacteristic).join(
+                Characteristic, 
+                Characteristic.id == ItemCharacteristic.characteristic_id
+            ).where(
+                ItemCharacteristic.item_id == id
+            ),
+            with_result="all"
+        )
+
+        result = ItemResponse(
+            id=item.id,
+            name=item.name,
+            description=item.description,
+            price=item.price,
+            category=ItemCategoryResponse.model_validate(category),
+            created_at=item.created_at,
+            updated_at=item.updated_at,
+            reviews=[ReviewResponse.model_validate(review) for review in reviews],
+            characteristics=[CoreResponse.model_validate(characteristic) for characteristic in characteristics]
+        )
+
+        return result
