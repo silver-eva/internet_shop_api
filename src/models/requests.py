7c2fb7093798a95fa591a8d0da480f799c92dba5
silver-eva <haushki.74.7@gmail.com>
2024-12-28 00:29:23 +0200

    feat: request/response models

--- src/models/requests.py
+++ src/models/requests.py
@@ -1,13 +1,31 @@
 from pydantic import BaseModel, Field
 from uuid import uuid4
 
+from typing import Optional, List, Literal
+
 class ListRequest(BaseModel):
     page: int = Field(default=1, gt=0)
     limit: int = Field(default=10, gt=0)
-    keyword: str = Field(default="")
+    keyword: Optional[str] = Field(default="", max_length=128)
 
 class CoreRequest(BaseModel):
-    id: str = Field(..., default_factory=uuid4)
-    name: str = Field(..., max_length=128)
-    description: str = Field(..., max_length=256)
+    id: Optional[str] = Field(..., default_factory=uuid4)
+    name: Optional[str] = Field(default="", max_length=128)
+    description: Optional[str] = Field(default="", max_length=256)
 
+class CharacteristicRequest(CoreRequest):
+    id: str = Field(default_factory=uuid4)
+    value: str = Field(..., max_length=128)
+
+class ItemRequest(CoreRequest):
+    category_id: str = Field(..., default_factory=uuid4)
+    price: float = Field(..., gt=0)
+    characteristics: list[CharacteristicRequest] = Field(..., default_factory=list, max_length=10)
+
+class ItemFilterRequest(ListRequest):
+    category: Optional[str] = Field(..., default_factory=uuid4)
+    min_price: Optional[float] = Field(gt=0, default=0)
+    max_price: Optional[float] = Field(gt=0, default=0)
+    characteristics: Optional[List[CharacteristicRequest]] = Field(default_factory=list)
+    order_by: Optional[Literal["name", "price", "created_at", "updated_at"]] = Field(default="price")
+    order_dir: Optional[Literal["asc", "desc"]] = Field(default="asc")
--- src/models/responses.py
+++ src/models/responses.py
@@ -18,3 +18,28 @@ class ListResponse(BaseModel):
     limit: int
     total: int
     items: list[CoreResponse]
+
+class RewiewResponse(CoreResponse):
+    name: str | None
+    stars: int
+
+class ItemCategoryResponse(CoreResponse):
+    id: UUID
+    name: str
+
+class ItemForListResponse(CoreResponse):
+    category: ItemCategoryResponse
+    price: float
+
+
+class ItemResponse(ItemForListResponse):
+    price: float
+    characteristics: list[CoreResponse]
+    reviews: list[RewiewResponse]
+
+class ItemListResponse(ListResponse):
+    items: list[ItemForListResponse]
+
+class ReviewResponse(CoreResponse):
+    name: str | None
+    stars: int
