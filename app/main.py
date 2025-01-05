51697f3cda8e64186ca0256ba3f3ce0468601905
silver-eva <haushki.74.7@gmail.com>
2025-01-05 19:23:59 +0200

    refactor: move startup acording to new layout

---
+++ app/main.py
@@ -0,0 +1,34 @@
+import uvicorn
+import argparse
+
+from app import app
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Process some arguments.")
+    parser.add_argument('-a', '--address', type=str, help='IP address to bind to', default='0.0.0.0')
+    parser.add_argument('-p', '--port', type=int, help='Port number to bind to', default=8000)
+    parser.add_argument('--reload', action='store_true', help='Enable auto-reload for development', default=False)
+
+    parser.add_argument('-l', '--log_level', type=str, choices=['debug', 'info', 'warning', 'error'], help='Logging level', default='info')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+
+    async def startup_event():
+        print("Startup")
+
+    async def shutdown_event():
+        print("Shutdown")
+    
+    app.add_event_handler("startup", startup_event)
+    app.add_event_handler("shutdown", shutdown_event)
+
+    uvicorn.run(
+        "main:app",
+        host=args.address,
+        port=args.port,
+        reload=args.reload,
+        log_level=args.log_level
+    )
